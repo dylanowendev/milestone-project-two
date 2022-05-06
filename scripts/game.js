4cb9f0a97f1b8c97a4bfd5390254d6a708704a42
dylanowendev <DylanLJOwen@gmail.com>
2022-05-06 18:36:32 +0000

    Created placeholder HTML cards and formatted them via CSS

---
+++ scripts/game.js
@@ -0,0 +1,5 @@
+// Placeholder script for CSS styling of cards, will be called in a function later //
+
+var card = document.createElement("li");
+card.setAttribute('class', 'p-card');
+board-bg.appendChild(p-card);
