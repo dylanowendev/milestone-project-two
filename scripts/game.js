e6d34730f23dd238e0cae456276d95a464aa0297
dylanowendev <DylanLJOwen@gmail.com>
2022-05-07 17:48:52 +0000

    Tweaked HTML, added additional CSS and started configuring Media Queries

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,1 @@
-// Placeholder script for CSS styling of cards, will be called in a function later //
 
-var card = document.createElement("li");
-card.setAttribute('class', 'p-card');
-board-bg.appendChild(p-card);
---
+++ scripts/game.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { };
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
